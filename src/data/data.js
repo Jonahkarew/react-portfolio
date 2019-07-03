a5bca1b73b5fcf57e8c0a817ea6fe72ee371f356
jonah <jonahkarew@gmail.com>
2019-07-03 16:22:29 -0400

    more project data

--- src/data/data.js
+++ src/data/data.js
@@ -16,34 +16,43 @@ const data = {
             'title': 'Scrape It!',
             'link': 'https://jonahkarew.github.io/project1_master/',
             'repo': 'https://github.com/Jonahkarew/project1_master',
+            'description': ''
         },
         {
             '_id':57285,
             'index': 0,
-            'image' : '../assets/images/',
-            'title': '',
-            'repo': '',
+            'image' : '../assets/images/ScrapeIt.PNG',
+            'title': 'NoteTaker',
+            'link': 'https://jonah-karew-note-taker.herokuapp.com/',
+            'repo': 'https://github.com/Jonahkarew/note-taker',
+            'description': ''
         },
         {
             '_id':57286,
             'index': 0,
-            'image' : '../assets/images',
-            'title': '',
-            'repo': '',
+            'image' : '../assets/images/MemoryGame.PNG',
+            'title': 'Pokemon Memory Game',
+            'link': 'https://jonahkarew.github.io/memory-game/',
+            'repo': 'https://github.com/Jonahkarew/memory-game',
+            'description': ''
         },
         {
             '_id':57287,
             'index': 0,
             'image' : '../assets/images',
             'title': '',
+            'link': '',
             'repo': '',
+            'description': ''
         },
         {
             '_id':57288,
             'index': 0,
             'image' : '../assets/images',
             'title': '',
+            'link': '',
             'repo': '',
+            'description': ''
         },
     ]
 }
